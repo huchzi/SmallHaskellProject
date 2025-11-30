a13f76360c66c6097aac2ad3fcd9b2f33bf16aa2
huchzi <c.huchzermeyer@posteo.de>
2025-11-30 21:22:14 +0000

    Finished lattice.

--- src/Svg.hs
+++ src/Svg.hs
@@ -26,6 +26,12 @@ type Eccentricity = Int
 clockToDegree :: Clock -> Int
 clockToDegree clock = 30 * (clock - 3) + 90
 
+polarToCartesian :: Int -> Eccentricity -> (Int, Int)
+polarToCartesian angle ecc = (round x, round y)
+  where x = radius * (cos ((fromIntegral angle) * pi / 180)) + (read centerX)
+        y = radius * (sin ((fromIntegral angle) * pi / 180)) + (read centerY)
+        radius = fromIntegral ecc
+
 eccentricityToPixels :: Eccentricity -> Int
 eccentricityToPixels ecc = div (ecc * oraDistance) 120
 
@@ -87,13 +93,33 @@ circleDef ecc =
                                                                              , ("cy", T.pack centerY)
                                                                              ]) [] ]
 
+latticeArcs :: Clock -> Int -> Element
+latticeArcs clockDifference radius = 
+  Element
+    "{http://www.w3.org/2000/svg}g"
+    mempty
+    (map (NodeElement . arcElement) [innerArc, outerArc, startArc, stopArc])
+  where arcElement arcPath = Element "{http://www.w3.org/2000/svg}path"  (M.fromList [ ("d", arcPath), ("stroke", "black"), ("fill", "none"), ("stroke-width", "2")]) []
+        innerArc = T.intercalate "" ["M ", T.show x1, " ", T.show y1, " A ", T.show radius, " ", T.show radius, " 0 0 1 ", T.show x2, " ", T.show y2]
+        outerArc = T.intercalate "" ["M ", T.show x3, " ", T.show y3, " A ", T.show (radius - 16), " ", T.show (radius - 16), " 0 0 1 ", T.show x4, " ", T.show y4]
+        startArc = T.intercalate "" ["M ", T.show x1, " ", T.show y1, " A 8 8 0 0 0 ", T.show x3, " ", T.show y3]
+        stopArc  = T.intercalate "" ["M ", T.show x2, " ", T.show y2, " A 8 8 0 0 1 ", T.show x4, " ", T.show y4]          
+        (x2, y2) = polarToCartesian stopAngle radius
+        (x1, y1) = polarToCartesian startAngle radius
+        (x4, y4) = polarToCartesian stopAngle (radius - 16)
+        (x3, y3) = polarToCartesian startAngle (radius - 16)
+        startAngle = 270 - div (clockDifference * 360) 24
+        stopAngle = 270 + div (clockDifference * 360) 24
+
 latticeElement :: Clock -> Eccentricity -> Element
 latticeElement clockDifference ecc = 
   Element
     "{http://www.w3.org/2000/svg}g"
     mempty
     [ NodeElement $ circleDef ecc
-    , NodeElement $ textElement ]
+    , NodeElement $ textElement 
+    , NodeElement $ latticeArcs clockDifference (eccentricityToPixels ecc) 
+    ]
   where textElement = Element "{http://www.w3.org/2000/svg}text" (M.fromList [("font-family", "sans-serif"), ("font-size", "20")]) [ NodeElement textPathElement ]
         textPathElement = Element "{http://www.w3.org/2000/svg}textPath" (M.fromList [("href", "#circularPath"), ("startOffset", offsetString), ("method", "align"), ("side","right")]) [ NodeContent latticeText ]
         offset = 25 - div (clockDifference * 100) 24  
@@ -247,7 +273,8 @@ fundusDrawing =
   Document
     (Prologue [] Nothing [])
     (Element svgName fundusDocAttributes [ NodeElement $ fundusTemplate LeftEye 
-                                         , NodeElement $ transformElement (latticeElement 2 $ retinalZones M.! "Equatorial") $ (rotateTransformation 3)
+                                         , NodeElement $ transformElement (latticeElement 1 $ retinalZones M.! "Equatorial") $ (rotateTransformation 3)
+                                         , NodeElement $ transformElement (latticeElement 2 $ retinalZones M.! "Equatorial") $ (rotateTransformation 6)
                                          , NodeElement $ transformElement tearElement $ (rotateTransformation 3) <> (translateTransformation $ retinalZones M.! "Equatorial")
                                          -- , NodeElement $ latticeElement 1 5 $ retinalZones M.! "Equatorial"
                                          ])
