f812a6faf0a2040d58af6bd3336ca5762180c83b
huchzi <c.huchzermeyer@posteo.de>
2025-12-02 05:21:29 +0000

    Started productive JSON.

--- app/Main.hs
+++ app/Main.hs
@@ -5,36 +5,25 @@ module Main (main) where
 
 import Lib
 import Svg
+import Json
 import GHC.Generics (Generic)
 import qualified Data.ByteString.Lazy as B
 import Data.Aeson
 import Text.XML
 import qualified Data.Map        as M
 
-data Example = Example 
-    { exampleType :: String,
-      exampleList :: [String]
-    } deriving (Show, Generic)
-
-instance FromJSON Example
-
 main :: IO ()
 main = do
 
     -- Eine JSON-Datei einlesen
     jsonData <- B.readFile "data/example.json"
-    print jsonData
 
     -- Die JSON-Datei in den Datentyp Example parsen
-    let example = decode jsonData :: Maybe Example
+    let example = decode jsonData :: Maybe Fundus
+    
     case example of
-        Just ex -> print ex
+        Just f -> print $ eyeSide f
         Nothing -> putStrLn "JSON Parsing failed."
-
-    -- Etwas mit diesen Daten machen
-    case example of
-        Just ex -> mapM_ print $ zip (map show [1..]) (exampleList ex)
-        Nothing -> putStrLn "JSON Parsin failed."
 
     -- Ein SVG ausgeben
     Text.XML.writeFile def "data/fundus.svg" fundusDrawing
---
+++ src/Json.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Json (Fundus, eyeSide, tears, equatorial) where
+
+import Lib
+import Svg
+import GHC.Generics (Generic)
+import qualified Data.ByteString.Lazy as B
+import Data.Aeson
+import Text.XML
+import qualified Data.Map        as M
+
+
+data Fundus = Fundus 
+    { eyeSide :: String
+    , tears :: [(String, Int, Int)]
+    , equatorial :: [(String, Int, Int, Int)]
+    } deriving (Show, Generic)
+
+instance FromJSON Fundus
