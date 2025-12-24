9b9e08d72bc52270838318655f81554babb69e45
huchzi <c.huchzermeyer@posteo.de>
2025-12-24 09:06:17 +0000

    Started servant API server, only POST result.

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
 
 module Main (main) where
 
@@ -13,6 +16,39 @@ import Data.Aeson
 import Text.XML
 import qualified Data.Map        as M
 
+import Servant
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Data.Text (Text)
+import Data.Text.Lazy (toStrict)
+
+type API = "trigger" :> Post '[PlainText] Text
+
+data Result = Result { resultText :: Text }
+  deriving (Show)
+
+server :: Server API
+server = postTrigger
+  where
+    postTrigger :: Handler Text
+    postTrigger = return resultValue
+
+app :: Application
+app = serve (Proxy :: Proxy API) server
+
+resultValue :: Text
+resultValue =  
+  toStrict $ Text.XML.renderText 
+              def 
+              (fundusDrawing LeftEye  [ detachmentGroup [detachmentElement 3, detachmentElement 4]
+                                      , encirclingBandElement
+                                      , latticeElement 3 (eccentricity Equatorial) 2 
+                                      , tearElement 3 $ eccentricity PreEquatorial
+                                      , roundHoleElement 11 $ eccentricity Anterior
+                                      , cobbleStoneElement 6 $ eccentricity PreEquatorial
+                                      , laserLesionElement 7 $ eccentricity PreEquatorial
+                                      ])
+
 main :: IO ()
 main = do
     jsonData <- B.readFile "data/drawing1.json"
@@ -22,13 +58,5 @@ main = do
         Just f ->  Text.XML.writeFile def "data/parsedFundus.svg" $ jsonToSvg f
         Nothing -> putStrLn "JSON Parsing failed."
 
-    -- Ein SVG ausgeben
-    Text.XML.writeFile def "data/fundus.svg" $ fundusDrawing LeftEye  [ detachmentGroup [detachmentElement 3, detachmentElement 4]
-                                                                      , encirclingBandElement
-                                                                      , latticeElement 3 (eccentricity Equatorial) 2 
-                                                                      , tearElement 3 $ eccentricity Equatorial
-                                                                      , roundHoleElement 11 $ eccentricity Anterior
-                                                                      , cobbleStoneElement 6 $ eccentricity PreEquatorial
-                                                                      , laserLesionElement 7 $ eccentricity PreEquatorial
-                                                                      ]
+    run 8080 app
     
